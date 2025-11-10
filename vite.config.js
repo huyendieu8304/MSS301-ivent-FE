d6dd627fa2bbafbfe473eec7a7eb3b083766b263
BuiMinhChien <chien181004@gmail.com>
2025-11-10 22:32:35 +0700

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+//
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000, // Cấu hình cổng tại đây
+//   },
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000, // Cấu hình cổng tại đây
-  },
+    plugins: [react()],
+    server: {
+        port: 3000, // vẫn giữ nguyên cho dev
+    },
+    base: '/', // quan trọng: đảm bảo tất cả route map về index.html
+    build: {
+        outDir: 'dist', // thư mục output cho Vercel
+    },
 })
